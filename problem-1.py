5f608118348f4124c1987dbf27ad0865ebc496dd
NayakaZN <97321138+NayakaZN@users.noreply.github.com>
2023-02-13 11:30:41 +0700

    Update problem-1.py

--- problem-1.py
+++ problem-1.py
@@ -3,3 +3,7 @@ for i in (1, 1000):
     if i % 3 == 0 or i % 5 == 0:
         summa += i
 print(summa)
+
+# I think I understood list comprehension more! Here's my improvement:
+
+print(sum(i for i in range(1, 1000) if i % 3 == 0 or i % 5 == 0))
