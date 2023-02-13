087211b82cc959d3fb45ffbde984134accc42ec3
NayakaZN <97321138+NayakaZN@users.noreply.github.com>
2023-02-13 11:07:32 +0700

    Update problem-2.py

--- problem-2.py
+++ problem-2.py
@@ -2,8 +2,16 @@ a, b, fib, even = 1, 1, [], []
 while a + b < 4000000:
     fib.append(a + b)
     a, b = b, a + b
-
 for x in fib:
     if x % 2 == 0:
         even.append(x)
 print(sum(even))
+
+# I improved it a bit...
+
+a, b, c = 1, 1, 0
+while (a + b) < 4000000:
+    a, b = b, (a + b)
+    if b % 2 == 0:
+        c += b
+print(c)
